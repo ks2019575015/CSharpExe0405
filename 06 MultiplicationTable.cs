8a8c5214d537f684a6b3858e6856c98e70e798c1
ks2019575015 <48661711+ks2019575015@users.noreply.github.com>
2021-04-04 21:27:09 +0900

    Add files via upload

---
+++ 06 MultiplicationTable.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace _0405
+{
+    class MultiplicationTable
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("구구단의 출력할 단수를 입력하세요 : ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= 9; i++)
+            {
+                Console.WriteLine("{0} x {1} = {2}", n, i, n * i);
+            }
+        }
+    }
+    }
---
+++ 15 TimesTable.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace _0405
+{
+    class TimesTable
+    {
+        static void Main(string[] args)
+        {
+            for (int y = 1; y <= 9; y++)
+            {
+                for (int x = 2; x <= 9; x++)
+                    Console.Write("{0}x{1}={2,2} ", x, y, x * y);
+                Console.WriteLine();
+            }
+        }
+        }
+}
